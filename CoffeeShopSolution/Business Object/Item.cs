9c6eaaa9fb549a6aa11c1e09bb0c19bc5f2697b9
iamapant <85598553+iamapant@users.noreply.github.com>
2023-03-03 22:19:27 +0700

    Add C# Solution

---
+++ CoffeeShopSolution/Business Object/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Object
+{
+    public class Item
+    {   
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public string? ItemDescription { get; set; }
+        public string? ItemType { get; set; }
+        public double ItemPrice { get; set; }
+        public int ItemStock { get; set; }
+    }
+}
